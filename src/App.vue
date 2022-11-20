8712ba3479c242ab30bf36447515cfabcc0b9713
Meathill <meathill@gmail.com>
2022-11-20 21:41:42 +0800

    change: init project

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import Phaser from 'phaser'
+import {onMounted, ref} from "vue";
+
+function preload () {
+  this.load.setBaseURL('https://labs.phaser.io');
+
+  this.load.image('sky', 'assets/skies/space3.png');
+  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+  this.load.image('blue', 'assets/particles/blue.png');
+}
+function create () {
+  this.add.image(400, 300, 'sky');
+
+  const particles = this.add.particles('blue');
+
+  const emitter = particles.createEmitter({
+    speed: 100,
+    scale: { start: 1, end: 0 },
+    blendMode: 'ADD'
+  });
+
+  const logo = this.physics.add.image(400, 100, 'logo');
+
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+
+  emitter.startFollow(logo);
+}
+
+const stage = ref<HTMLDivElement>();
+
+onMounted(() => {
+  const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: stage.value,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 200 }
+      }
+    },
+    scene: {
+      preload,
+      create,
+    },
+  };
+
+  const game = new Phaser.Game(config);
+});
+
+
+</script>
+
+<template>
+  <div ref="stage">
+  </div>
+</template>
